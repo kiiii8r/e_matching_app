284c1fb13335b788301f74e70350b1f79cdb8dfd
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-07 17:38:45 +0900

    userカラム 修正

--- db/migrate/20201007032402_devise_create_users.rb
+++ db/migrate/20201007032402_devise_create_users.rb
@@ -4,14 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
+      t.string :nickname,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_name_kana,    null: false
+      t.string :last_name_kana,     null: false
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.references :age_id,         null: false
-      t.references :gender_id,      null: false
-      t.references :prefecture_id,  null: false
-      t.references :language_id,    null: false
-      t.references :role_id,        null: false
+      t.string :password,           null: false, default: ""
+      t.references :age,            null: false
+      t.references :gender,         null: false
+      t.references :prefecture,     null: false
+      t.date :birth_day,            null: false
       t.string :image
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_07_032402) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.string "email", default: "", null: false
+    t.string "password", default: "", null: false
+    t.bigint "age_id", null: false
+    t.bigint "gender_id", null: false
+    t.bigint "prefecture_id", null: false
+    t.date "birth_day", null: false
+    t.string "image"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["age_id"], name: "index_users_on_age_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["gender_id"], name: "index_users_on_gender_id"
+    t.index ["prefecture_id"], name: "index_users_on_prefecture_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end

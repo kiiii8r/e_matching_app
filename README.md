2b2b182104e340bee83199dd059713acd379b5a2
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-07 17:33:43 +0900

    ER図、README 修正

--- README.md
+++ README.md
@@ -5,15 +5,18 @@
 | Column           | Type       | Options                        |
 | ---------------- | ---------- | ------------------------------ |
 | id               | integer    | null: false                    |
-| name             | string     | null: false                    |
+| nickname         | string     | null: false                    |
+| first_name       | string     | null: false                    |
+| last_name        | string     | null: false                    |
+| first_name_kana  | string     | null: false                    |
+| last_name_kana   | string     | null: false                    |
 | email            | string     | null: false                    |
 | password         | integer    | null: false                    |
 | age_id           | references | null: false, foreign_key: true |
 | gender_id        | references | null: false, foreign_key: true |
 | prefecture_id    | references | null: false, foreign_key: true |
-| language_id      | references | null: false, foreign_key: true |
-| role_id          | references | null: false, foreign_key: true |
-| image            | strings    |                                |
+| birth_day        | date       | null: false, foreign_key: true |
+| image            | string     |                                |
 
 ### Association
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_032402) do
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.bigint "age_id_id", null: false
-    t.bigint "gender_id_id", null: false
-    t.bigint "prefecture_id_id", null: false
-    t.bigint "language_id_id", null: false
-    t.bigint "role_id_id", null: false
-    t.string "image"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["age_id_id"], name: "index_users_on_age_id_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["gender_id_id"], name: "index_users_on_gender_id_id"
-    t.index ["language_id_id"], name: "index_users_on_language_id_id"
-    t.index ["prefecture_id_id"], name: "index_users_on_prefecture_id_id"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id_id"], name: "index_users_on_role_id_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end

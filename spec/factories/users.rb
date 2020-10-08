78718649f11e5a6182e17fe098414b9442baa19f
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-08 14:42:33 +0900

    gimei 導入、user faker記載

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
-FactoryBot.define do
+FactoryBot.defind do
   factory :user do
-    
+    nickname { Faker::Name.last_name }
+    first_name { Gimei.last.kanji }
+    last_name { Gimei.first.kanji }
+    first_name_kana { Gimei.last.katakana }
+    last_name_kana { Gimei.first.katakana }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    age  { Faker::Number.within(range: 1..100) }
+    gender { 'man' }
+    birth_day { Faker::Date.birthday }
   end
 end

816a9f77a31709b9cf0d82db27ca313c8f2598ca
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-09 11:58:29 +0900

    profile モデル ActiveHash 設置

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,6 @@
 class Profile < ApplicationRecord  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :language
+  belongs_to_active_hash :role
 end

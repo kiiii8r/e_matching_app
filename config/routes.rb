cc56ce5c90c07dfb3ef9a8e61c18647531387d28
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-10 15:28:11 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :profiles, only: [:new, :create, :edit, :update]
   end
+  resources :rooms, only: :index do
+    resources :messages, only: :create
+  end
 end

6cc385db9164170b38cdcd1b84766066ed17080e
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-10 15:45:11 +0900

    contoroller コード記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,7 @@
 class RoomsController < ApplicationController
+  def index
+    @message = Message.new
+    @room = Room.find(params[:user_id])
+    @messages = @room.messages.includes(:user)
+  end
 end

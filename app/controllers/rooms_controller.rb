464e4c51b4ee20c67fca9f268278d7fc83a0fbf6
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-10 17:44:44 +0900

    room関連

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,4 @@
 class RoomsController < ApplicationController
-  def show
-    @message = Message.new
-    @room = Room.find(params[:user_id])
-    @messages = @room.messages.includes(:user)
-  end
-
   def create
     if @room = Room.create
       @room_user = RoomUser.create
@@ -12,4 +6,12 @@ class RoomsController < ApplicationController
     else
       rende user_path
     end
+  end
+
+  def show
+    @message = Message.new
+    @room = Room.new
+    @messages = @room.messages.includes(:user)
+    @user = User.find(params[:id])
+  end
 end

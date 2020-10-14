7d86941bc4c72b7804d9f7b139f0f852faa87040
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-15 02:11:00 +0900

    ルーティング、コントローラ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :search_product, only: [:index, :search]
+
   def index
     @users = User.all.order('created_at DESC')
   end
@@ -11,6 +13,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @profile = Profile.find(params[:id])
     @room_number = RoomUser.where(user_id: current_user.id).pluck(:room_id) & RoomUser.where(user_id: @user.id).pluck(:room_id)
+  end
 
+  def search
+    @results = @p.result.includes(:profile)
+  end
+
+  private
+
+  def search_product
+    @p = User.ransack(params[:q])  # 検索オブジェクトを生成
   end
 end

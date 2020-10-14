e70a7cffa364bbc86138a3f3c1f2ae510219181d
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-14 17:48:10 +0900

    messageの投稿、partner_id関連設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to room_path(@room)
+      redirect_to room_path(@room, partner_id: params[:message][:partner_id] )
     else
        @messages = @room.messages.includes(:user)
        render room_path

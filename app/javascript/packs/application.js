dc1d48a2723e02d87e8bddee1b5c31cb62ff1ae4
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-20 21:31:36 +0900

    second header 途中固定 実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/activestorage").start();
 require("channels");
 require("../calculation_age");
 require("../scroll_end");
+require("../scroll_rock");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

cb07d2233febf990b679e074c181747a84476424
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-23 20:36:53 +0900

    message controller  notification保存追記

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
+      Notification.create(notice_params)
       redirect_to room_path(@room, partner_id: params[:message][:partner_id])
     else
       @messages = @room.messages.includes(:user)
@@ -16,4 +17,7 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:message, :image).merge(user_id: current_user.id)
   end
+
+  def notice_params
+    params.require(visiter: current_user.id, visited: params[:partner_id], message_id: @message.id)
 end

f157ca987dd6171a6537fb3d44b2ceac734476e0
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-23 14:48:39 +0900

    notification model 記述

--- db/migrate/20201023053024_create_notifications.rb
+++ db/migrate/20201023053024_create_notifications.rb
@@ -1,6 +1,10 @@
 class CreateNotifications < ActiveRecord::Migration[6.0]
   def change
     create_table :notifications do |t|
+      t.references :visiter,  optional: true, foreign_key: true
+      t.references :visited,  optional: true, foreign_key: true
+      t.references :like,     optional: true, foreign_key: true
+      t.references :message,  optional: true, foreign_key: true
 
       t.timestamps
     end

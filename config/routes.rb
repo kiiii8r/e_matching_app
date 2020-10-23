e2b9650aaed384090df472dd1e08417dc62ae29c
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-23 18:49:03 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'users/search' 
   resources :users, only: [:index, :show] do
     resources :profiles, only: [:new, :create, :edit, :update]
+    resources :likes, only: :create
+    resources :notifications, only: [:index, :create]
   end
   resources :rooms, only: [:create, :show] do
     resources :messages, only: :create

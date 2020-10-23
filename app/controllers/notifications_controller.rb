93e611cad10b03027881f59939e6e0924c71b586
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-10-24 02:19:51 +0900

    通知降順

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = Notification.where(user_id: current_user.id)
+    @notifications = Notification.where(user_id: current_user.id).order(created_at: :desc)
   end
 
   def create

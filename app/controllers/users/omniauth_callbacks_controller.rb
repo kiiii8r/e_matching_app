5ae923916dc6b49eb13c1e348c74afaf893ff10d
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-11-09 01:09:46 +0900

    google facebook login API

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  
   def facebook
     authorization
-   end
+  end
   
-   def google_oauth2
+  def google_oauth2
     authorization
-   end
+  end
   
-   private
+  private
   
-   def authorization
+  def authorization
     sns_info = User.from_omniauth(request.env["omniauth.auth"])
     @user = sns_info[:user]
-
 
     if @user.persisted? 
       sign_in_and_redirect @user, event: :authentication
@@ -20,5 +20,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @sns_id = sns_info[:sns].id
       render template: 'devise/registrations/new'
     end
-   end
+  end
 end

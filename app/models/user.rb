46f44e2ad0e44e9e7e0d592186f78b5bd82597b5
kiiii8r <68574158+kiiii8r@users.noreply.github.com>
2020-11-09 00:10:07 +0900

    user.rb 編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,11 @@ class User < ApplicationRecord
       nickname: auth.info.name,
         email: auth.info.email
     )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
   end
 
   validates :nickname, presence: true, length: { maximum: 8 } 
